4bbfc72affb3f2ebcc8973c77b593152f790999a
Ryan Miller <rpmiller@udel.edu>
2021-09-29 19:45:30 -0400

    Final Funct

--- my-app/src/API.ts
+++ my-app/src/API.ts
@@ -1,3 +1,16 @@
+import StackUtils from "stack-utils";
+import { shuffleArray } from './utils';
+
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answer: string[];
+    question: string;
+    type: string;
+}
+
+export type QuestionState = Question & {answers: string[]};
 
 export enum Difficulty{
     EASY = "easy",
@@ -8,5 +21,12 @@ export enum Difficulty{
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint ='https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple';
     const data = await (await fetch(endpoint)).json();
-    console.log(data);
-}
+    return data.results.map((question: Question)=>(
+        {
+            ...question, 
+            answers: shuffleArray([...question.incorrect_answer, question.correct_answer,
+            ])
+
+        }
+    ));
+};
